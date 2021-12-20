4a3600a8169805eba4bcf76ba802debf12c9f462
Uehara YUTO <e215747@ie.u-ryukyu.ac.jp>
2021-12-20 11:54:39 +0900

    3 commit

--- Report5.java
+++ Report5.java
@@ -1,15 +1,9 @@
 public class Report5 {
     public static void main(String[] args) { 
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } 
-        catch (NullPointerException e){
-                System.out.println("NullPointerExceptionが発生しました");
-                System.out.println(e.getMessage());
-        }
-    }
-       
-}
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
 
+        System.out.println(value);
+    }
+}
 
